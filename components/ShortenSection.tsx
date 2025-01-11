623ab0ff2bf37bf739b984679e0951472e980859
IvanGadus <106027737+IvanGadus@users.noreply.github.com>
2025-01-11 11:14:23 +0100

    fix scolling position for shortening section

--- components/ShortenSection.tsx
+++ components/ShortenSection.tsx
@@ -7,7 +7,7 @@ export default function ShortenSection() {
 	const [links, setLinks] = useState<string[]>([]);
 	const [loading, setLoading] = useState(false);
 	return (
-		<div className="bg-primaryGrey w-full h-max flex flex-col relative overflow-clip">
+		<div className="bg-primaryGrey w-full h-max flex flex-col relative overflow-clip ">
 			<div className="relative flex flex-col">
 				<div className="absolute w-screen h-1/2 z-0 left-0 bg-white"></div>
 				<LinkInput setLinks={setLinks} loading={loading} />
